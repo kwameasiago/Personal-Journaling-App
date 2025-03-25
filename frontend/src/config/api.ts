d318cb8dd1c96cf4450fc813ad594a04e43d6571
kwame <kwameasiago@gmail.com>
2025-03-26 00:51:18 +0300

    feature: add auth wrapper for authentication
    - Implemented an authentication wrapper to secure protected routes and components
    - Integrated the auth wrapper with the existing routing setup for seamless user session management

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -4,25 +4,33 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost',
 });
 
-
 axiosInstance.interceptors.request.use(
   (config) => {
-    if(!config.url){
-      throw new Error('Config url is undefeined')
+    if (!config.url) {
+      throw new Error('Config url is undefined');
     }
     
+    // Attach token for all endpoints except /login and /register
     if (!config.url.includes('/login') && !config.url.includes('/register')) {
-      
       const token = localStorage.getItem('token');
       if (token) {
-        
         config.headers.Authorization = `Bearer ${token}`;
       }
     }
     return config;
   },
   (error) => {
-    
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 403) {
+      localStorage.removeItem('token'); 
+      window.location.href = '/login';  
+    }
     return Promise.reject(error);
   }
 );
